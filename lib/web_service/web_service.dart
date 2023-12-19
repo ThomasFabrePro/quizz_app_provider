ba9c8daa47efefe4bdbd84cde9e823c21c89f402
Thomas Fabre <thomas.fabre@outlook.fr>
2023-12-19 15:59:42 +0100

    distant server url

--- lib/web_service/web_service.dart
+++ lib/web_service/web_service.dart
@@ -3,6 +3,7 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class WebService {
+  // static const String baseUrl = 'https://b347-86-237-94-35.ngrok.io';
   static const String baseUrl = 'http://10.0.0.25:80';
 }
 
